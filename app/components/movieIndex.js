1e4360e90aa815964fbf998b48c15c099f233519
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 16:25:46 -0600

    testing the api

--- app/components/movieIndex.js
+++ app/components/movieIndex.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
-import MoviesApi from '../../apis/moviesapi.js';
+import React, { Component } from 'react'
+import MoviesApi from '../apis/moviesapi.js'
+import moviedbAPIKey from '../../keys'
 
 export default class MovieIndex extends Component {
   constructor() {
     super();
+    this.moviesApi = new MoviesApi(moviedbAPIKey)
+
+    this.state = {
+      latestMovies: [],
+    }
+  }
+
+  componentDidMount = async () => { 
+    const movies = await this.moviesApi.fetchLatestMovies()
+    debugger
   }
 
   render() {

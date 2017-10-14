074bb22069cdb18780d3421b8e5983e846d98a7c
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 13:45:55 -0600

    add sleep function to hack around fetchmock problems

--- app/components/__tests__/movieIndex.test.js
+++ app/components/__tests__/movieIndex.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import MovieIndex from '../movieIndex'
+import { sleep } from '../../../utils'
 
 describe('MovieIndex', () => {
   it('should match the snapshot', () => {

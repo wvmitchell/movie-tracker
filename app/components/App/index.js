a974a55ddbf733c0b458daef8a04bdb424697b2a
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 21:27:01 -0600

    add react router, and make first two routes

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import MovieIndex from '../MovieIndex';
+import React, { Component } from 'react'
+import { Route } from 'react-router'
+import MovieIndex from '../MovieIndex'
+import Login from '../Login'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 export default class App extends Component {
   render = () => (
     <MuiThemeProvider>
-      <MovieIndex />
+      <div>
+        <Route exact path='/' component={MovieIndex} />
+        <Route exact path='/login' component={Login} />
+      </div>
     </MuiThemeProvider>
   )
 }

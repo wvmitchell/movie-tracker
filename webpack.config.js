bac577ce0c3547f96d8600cd96f58e04822f331c
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 18:42:22 -0600

    update webpack config so sass is automatically loaded

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,22 @@ module.exports = {
      publicPath: '/app/'
    },
    module: {
-     loaders: [{
-       test: /.jsx?$/,
-       loader: 'babel-loader',
-       include: path.join(__dirname, 'app'),
-       exclude: /node_modules/,
-       options: {
-         presets: ['latest', 'react'],
-         plugins: ['babel-plugin-transform-class-properties']
-       }
-     }]
+     loaders: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          include: path.join(__dirname, 'app'),
+          exclude: /node_modules/,
+          options: {
+            presets: ['latest', 'react'],
+            plugins: ['babel-plugin-transform-class-properties']
+          }
+        },
+        {
+           test: /\.scss$/,
+           loaders: ["style-loader", "css-loader", "sass-loader"]
+        }
+     ]
    },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']

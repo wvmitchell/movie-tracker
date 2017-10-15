7421bf70a4ffc1a48fcc86dff42a01b1a878364a
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 21:25:10 -0600

    add shell component for login page

---
+++ app/components/Login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Login extends Component {
+  render = () => (
+    <div>Login</div>
+  )
+}
+
+Login.propTypes = {
+}
+
+export default Login

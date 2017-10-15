9f3761b399691e4314b0b216287c2d9cbd8eff94
Will Mitchell <wvmitchell@gmail.com>
2017-10-14 21:28:02 -0600

    create super minimal initial reducer and store, leaving it in index.js for now

--- app/index.js
+++ app/index.js
@@ -1,5 +1,32 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
 
-ReactDOM.render(<App/>, document.getElementById('main'))
+// Reducer
+const initialState = Object.assign({})
+const reducer = (state = initialState, action) => {
+  if (typeof state === 'undefined') {
+    return initialState
+  }
+  
+  // no actions to handle yet
+  
+  return state
+}
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('main')
+)
